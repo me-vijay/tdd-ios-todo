5a601e013af29e43bf070f075af12c1b9493203b
vijay kumar <vijay.kumar@vistajet.com>
2019-01-25 16:32:18 +0500

    implemented test driven data models

---
+++ TDD_iOS_ToDoTests/ItemManagerTests.swift
@@ -0,0 +1,124 @@
+//
+//  ItemManagerTests.swift
+//  ToDoTests
+//
+//  Created by vijay on 10/01/2019.
+//  Copyright © 2019 vijay. All rights reserved.
+//
+
+import XCTest
+@testable import TDD_iOS_ToDo
+import CoreLocation
+
+class ItemManagerTests: XCTestCase {
+    var sut : ItemManager!
+    
+    override func setUp() {
+        super.setUp()
+        sut = ItemManager()
+    }
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+    
+    func test_ToDoCount_InitiallyIsZero() {
+        XCTAssertEqual(sut.toDoCount, 0)
+    }
+    
+    func test_DoneCount_InitiallyIsZero() {
+        XCTAssertEqual(sut.doneCount, 0)
+    }
+    
+    func test_AddCount_IncreasesToDoCountByOne() {
+        let oldCount = sut.toDoCount
+        
+        sut.add(ToDoItem(title: ""))
+        
+        XCTAssertEqual(sut.toDoCount, oldCount + 1, "new count of toDoItems is invalid")
+    }
+    
+    func test_ItemAt_ReturnsAddedItem() {
+        let item = ToDoItem(title: "Foo")
+        sut.add(item)
+        
+        let returnedItem = sut.item(at:sut.toDoCount-1)
+        
+        XCTAssertEqual(returnedItem.title, item.title, "returned item is not same as added")
+    }
+    
+    func test_CheckItemAt_ChangesCount() {
+        sut.add(ToDoItem(title: ""))
+        let oldDoneCount = sut.doneCount
+        let oldToDoCount = sut.toDoCount
+        
+        _ = sut.checkItem(at: 0)
+        
+        XCTAssertEqual(sut.doneCount, oldDoneCount+1, "checked items count is invalid")
+        XCTAssertEqual(sut.toDoCount, oldToDoCount-1, "todo items count is invalid")
+    }
+    
+    func test_CheckItemAt_RemovesCorrectItemFromToDoItems() {
+        //arrange
+        let firstItem = ToDoItem(title: "First")
+        let secondItem = ToDoItem(title: "Second")
+        let oldDoneCount = sut.doneCount
+        sut.add(firstItem)
+        sut.add(secondItem)
+        
+        //act
+        let checkedItem = sut.checkItem(at: 0)
+        
+        //assert
+        XCTAssertEqual(checkedItem.title, firstItem.title, "incorrect todo item removed")
+        XCTAssertEqual(sut.item(at: 0).title, secondItem.title, "incorrect next todo item")
+        XCTAssertEqual(sut.doneCount, oldDoneCount+1, "checked item not added to done items")
+        XCTAssertEqual(sut.doneItem(at: sut.doneCount-1).title, firstItem.title, "incorrect checked item appended to done items")
+    }
+    
+    func test_DoneItemAt_ReturnsCheckedItem() {
+        let item = ToDoItem(title: "Foo")
+        sut.add(item)
+        _ = sut.checkItem(at: 0)
+        
+        let checkedItem = sut.doneItem(at: 0)
+        
+        XCTAssertEqual(checkedItem.title, item.title, "")
+    }
+    
+    
+    func test_RemoveAll_MakesCountsZero() {
+        
+        let item1 = ToDoItem(title: "Foo")
+        let item2 = ToDoItem(title: "Bar")
+        sut.add(item1)
+        sut.add(item2)
+        _ = sut.checkItem(at: 0)
+        
+        XCTAssertEqual(sut.toDoCount, 1)
+        XCTAssertEqual(sut.doneCount, 1)
+        
+        sut.removeAll()
+        
+        XCTAssertEqual(sut.toDoCount, 0)
+        XCTAssertEqual(sut.doneCount, 0)
+    }
+    
+    
+    //MARK: Ensuring Uniqueness
+    func test_Add_WhenSimilarItemIsAlreadyAdded_DoesNotIncreaseCount() {
+        sut.add(ToDoItem(title: "Item1",
+                         itemDescription: "description",
+                         timeStamp: 1.0,
+                         location: Location(name: "location", coordinates: CLLocationCoordinate2D(latitude: 1.0, longitude: 2.0))
+        ))
+        sut.add(ToDoItem(title: "Item1",
+                         itemDescription: "description",
+                         timeStamp: 1.0,
+                         location: Location(name: "location", coordinates: CLLocationCoordinate2D(latitude: 1.0, longitude: 2.0))
+        ))
+        XCTAssertEqual(sut.toDoCount, 1)
+
+    }
+}
---
+++ TDD_iOS_ToDoTests/ToDoItemTests.swift
@@ -0,0 +1,133 @@
+//
+//  ToDoItemTests.swift
+//  ToDoTests
+//
+//  Created by vijay on 15/12/2018.
+//  Copyright © 2018 vijay. All rights reserved.
+//
+
+import XCTest
+import CoreLocation
+
+@testable import TDD_iOS_ToDo
+
+class ToDoItemTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func test_Init_WhenGivenTitle_SetsTitle() {
+        //arrange
+        let title = "Foo"
+        
+        //act
+        let item = ToDoItem(title: title)
+        
+        //assert
+        XCTAssertEqual(item.title, title, "should set title")
+    }
+    
+    func test_Init_WhenGivenDescription_SetsDescription() {
+        //arrange
+        let description = "Bar"
+        
+        //act
+        let item = ToDoItem(title: "Foo",
+                     itemDescription: description)
+
+        //assert
+        XCTAssertEqual(item.itemDescription, description, "should set description")
+    }
+    
+    func test_Init_setsTimeStamp() {
+        //arrange
+        let timeStamp = 0.0
+        
+        //act
+        let item = ToDoItem(title: "",
+                            timeStamp: timeStamp)
+        
+        //assert
+        XCTAssertEqual(item.timeStamp, timeStamp, "should set timestamp")
+    }
+    
+    func test_Init_setsLocation() {
+        //arrange
+        let location = Location(name: "Foo")
+        
+        //act
+        let item = ToDoItem(title: "", location: location)
+        
+        //assert
+        XCTAssertEqual(item.location?.name, location.name, "should set location")
+    }
+  
+    // MARK: Equatable tests
+    func test_EqualItems_AreEqual() {
+        //arrange
+        let first = ToDoItem(title: "Item1",
+                             itemDescription: "description",
+                             timeStamp: 1.0,
+                             location: Location(name: "location", coordinates: CLLocationCoordinate2D(latitude: 1.0, longitude: 2.0))
+        )
+        
+        let second = ToDoItem(title: "Item1",
+                              itemDescription: "description",
+                              timeStamp: 1.0,
+                              location: Location(name: "location", coordinates: CLLocationCoordinate2D(latitude: 1.0, longitude: 2.0))
+        )
+        
+        //act & assert
+        XCTAssertEqual(first, second)
+    }
+    
+    func test_Items_WhenLocationsDiffer_AreNotEqual() {
+        //arrange
+        let first = ToDoItem(title: "",
+                             location: Location(name: "Foo"))
+        let second = ToDoItem(title: "",
+                              location: Location(name: "Bar"))
+        
+        //act & assert
+        XCTAssertNotEqual(first, second)
+    }
+    
+    func test_Items_WhenOneLocationIsNil_AreNotEqual() {
+        var first = ToDoItem(title: "", location: Location(name: "Foo"))
+        
+        var second = ToDoItem(title: "", location: nil)        
+        XCTAssertNotEqual(first, second)
+        
+        first = ToDoItem(title: "", location: nil)
+        second = ToDoItem(title: "", location: Location(name: "Foo"))
+        XCTAssertNotEqual(first, second)
+    }
+    
+    func test_Items_WhenTimeStampsDiffer_AreNotEqual() {
+        let first = ToDoItem(title: "Foo", timeStamp: 1.0)
+        let second = ToDoItem(title: "Foo", timeStamp: 2.0)
+        
+        XCTAssertNotEqual(first, second)
+    }
+    
+    func test_Items_WhenItemDescriptionsDiffer_AreNotEqual() {
+        let first = ToDoItem(title: "Foo", itemDescription: "first Item")
+        let second = ToDoItem(title: "Foo", itemDescription: "second Item")
+        
+        XCTAssertNotEqual(first, second)
+    }
+    
+    func test_Items_WhenTitlesDiffer_AreNotEqual() {
+        let first = ToDoItem(title: "Foo")
+        let second = ToDoItem(title: "Bar")
+        
+        XCTAssertNotEqual(first, second)
+    }
+}
